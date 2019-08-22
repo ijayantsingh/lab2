fee5d4c20988b9090f72d495aa96cf1debab77f4
ijayantsingh <jayant.singh_cs18@gla.ac.in>
2019-08-22 15:00:12 +0530

    add file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author SAGAR THAKUR
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner
+    }
+}
